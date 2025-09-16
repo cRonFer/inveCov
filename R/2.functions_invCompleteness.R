38339e5a1c5ffc86e451257a5844b3d9d4e5a4d8
cRonFer <cristinaronquillo@outlook.com>
2025-09-16 12:21:39 +0200

    1609

--- R/2.functions_invCompleteness.R
+++ R/2.functions_invCompleteness.R
@@ -1,13 +1,13 @@
 ## Functions ####
-inventory_completeness <- function(data = data,
-                                   dir_e = 'invComplAnalysis',
-                                   resolution = 100, # grid resolution
-                                   cell_shape = NULL,  # shape of cell ('hex', 'square') if NA square
-                                   Records_t = 15,
-                                   Ratio_t = 5,
-                                   Slope_t = 0.1,
-                                   Completeness_t = 75,
-                                   crs = 4326){
+inventory_completeness <- function(data,
+                                   dir_e,
+                                   resolution, # grid resolution
+                                   cell_shape,  # shape of cell ('hex', 'square') if NA square
+                                   Records_t,
+                                   Ratio_t,
+                                   Slope_t,
+                                   Completeness_t,
+                                   crs){
   # Plot map of occurrences by Gen information ####
   occ_gen_map <- function(data1, title){
     map_points <- ggplot() +
@@ -34,7 +34,7 @@ inventory_completeness <- function(data = data,
             axis.title = element_text(size = 10, face = "bold"))
   }
   #. 4. Plot of inv. completeness values map
-  complt_plotMap <- function(comp_shp,estWS, title){
+  complt_plotMap <- function(title){
     ggplot() +
       geom_sf(data = study_area_pol, fill = 'lightgrey', color = 'darkgrey') +
       geom_sf(data = comp_shp, aes(fill = Completeness), color = "transparent") +
@@ -54,8 +54,22 @@ inventory_completeness <- function(data = data,
   data(adworld) # knowBR needs add world polygon to work
 
   # Create working directory
-  dir.create(paste0('output/',dir_e))
-  here::here(paste0('output/',dir_e))
+  create_and_set_directory <- function(dir_e) {
+    # Create directory if it doesn't exist
+    if (!dir.exists(dir_e)) {
+      dir.create(dir_e, recursive = TRUE)
+      message(paste("Directory created:", dir_e))
+    } else {
+      message(paste("Directory already exists:", dir_e))
+    }
+
+    # Set the working directory using here::here()
+    setwd(dir_e)
+    message(paste("Working directory set to:", getwd()))
+
+    return(getwd())
+    }
+  create_and_set_directory(dir_e)
 
   # Create grid
   grid <- create_grid(resolution = resolution,
@@ -86,12 +100,13 @@ inventory_completeness <- function(data = data,
                dec = ".")
   # Check the estimators from knowBR output:
   est <- read.csv('Estimators.CSV', header = TRUE, sep = ",")
+  print(paste('There are ', nrow(est), ' well surveyed cells in the study area based on the chosen thresholds'))
   # Distribution of completeness values by other estimators:
   p1 <- threshold_plot(est$Records, Records_t, 'Records') + ylab('Completeness')
   p2 <- threshold_plot(est$Ratio, Ratio_t, 'Ratio')
   p3 <- threshold_plot(est$Slope, Slope_t, 'Slope')
   completn_thresholds <- p1|p2|p3
-  ggsave('_completeness_thresholds.png', completn_thresholds,
+  ggsave('completeness_thresholds.png', completn_thresholds,
          width = 12, height = 5)
   # plot completeness values study area map
   comp_shp <- merge(grid, est, by.x = 'cell_id', by.y = 'Area', all.x = TRUE) # Write here 'by.x' = the unique identifier name of your grid shapefile
@@ -99,16 +114,13 @@ inventory_completeness <- function(data = data,
   comp_shp <- comp_shp %>%
     filter(!is.na(Completeness)) %>%
     st_transform(crs)
-  complt_map <- complt_plotMap(comp_shp, dir_e)
-  ggsave('_completeness_Map.png', complt_map, dpi = 500,
-         width = 6, height = 8)
   # Filter est dataset selecting WELL-SURVEY CELLS:
   estWS <- comp_shp %>% filter(Records >= Records_t) %>%
     filter(Ratio >= Ratio_t) %>%
     filter(Slope <= Slope_t) %>%
     filter(Completeness >= Completeness_t)
   # Map of Completeness
-  WS_map <- complt_plotMap(estWS,dir_e)
+  WS_map <- complt_plotMap(dir_e)
   ggsave('wellSurveyCells_Map.png', WS_map, dpi = 500, width = 6, height = 8)
 
   # Extract centroids of Well Survey cells for the environmental Space analysis
@@ -117,6 +129,6 @@ inventory_completeness <- function(data = data,
                                        lat = sf::st_coordinates(.)[,2]) %>%
     dplyr::select(c(cell_id, lon, lat))
   WS_cent_dt$geometry <- NULL
-  fwrite(WS_cent_dt, '_centroids.csv', sep = ";")
+  fwrite(WS_cent_dt, 'WS_centroids.csv', sep = ";")
 }
 
